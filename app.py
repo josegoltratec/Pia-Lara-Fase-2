fadf3e0be7190009426c062b190bd894bda9afe8
Jose Manuel <w@goltratec.com>
2023-06-02 18:25:52 +0200

    corrijo variables

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ from pydub import AudioSegment
 app = Flask(__name__)
 
 MODEL_PATH = "model"
-rutaAudio = "audio_files/"
+ruta_audio = "audio_files/"
 processor = AutoProcessor.from_pretrained(MODEL_PATH)
 model = AutoModelForSpeechSeq2Seq.from_pretrained(MODEL_PATH)
 forced_decoder_ids = processor.get_decoder_prompt_ids(language="spanish", task="transcribe")
@@ -40,17 +40,15 @@ def process_audio():
         os.makedirs('audio_files')
 
     name_audio = str(uuid.uuid4()) + '.wav'
-    audioPath = rutaAudio + name_audio
+    audio_path = ruta_audio + name_audio
 
     audio_file.save(os.path.join('audio_files', name_audio))
-    song = AudioSegment.from_file(rutaAudio + name_audio)
-    song.export(audioPath, format="wav")
-    data, s = librosa.load(audioPath, sr=16000) 
+    song = AudioSegment.from_file(audio_path)
+    song.export(audio_path, format="wav")
+    data, s = librosa.load(audio_path, sr=16000) 
     input_features = processor(data, sampling_rate=16000, return_tensors="pt").input_features 
     predicted_ids = model.generate(input_features, forced_decoder_ids=forced_decoder_ids)
     transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
-
-    print(transcription)
 
     return transcription
 
